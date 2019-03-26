a464751bab8dd9c0e54d27f02936f1825900d41a
millart <millart@rose-hulman.edu>
2019-03-26 00:20:38 -0400

    DONE

--- src/m1_Point.py
+++ src/m1_Point.py
@@ -1107,7 +1107,7 @@ def run_test_halfway_to():
 
     """
     # -------------------------------------------------------------------------
-    # TODO: 13.  Follow the same instructions as in TO-DO 3 above,
+    # DONE: 13.  Follow the same instructions as in TO-DO 3 above,
     #    but for the  halfway_to  method specified above.
     # -------------------------------------------------------------------------
     print()
